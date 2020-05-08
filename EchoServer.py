478ca886524890cadabf59d37cc8de584c0cca0f
Arturr0 <41492101+Arturr0@users.noreply.github.com>
2020-05-08 11:35:01 +0200

    init

--- EchoServer.py
+++ EchoServer.py
@@ -20,26 +20,6 @@ class SimpleEcho(WebSocket):
    def handleClose(self):
       pass
 
-clients = []
-class SimpleChat(WebSocket):
-
-   def handleMessage(self):
-      for client in clients:
-         if client != self:
-            client.sendMessage(self.address[0] + u' - ' + self.data)
-
-   def handleConnected(self):
-      print (self.address, 'connected')
-      for client in clients:
-         client.sendMessage(self.address[0] + u' - connected')
-      clients.append(self)
-
-   def handleClose(self):
-      clients.remove(self)
-      print (self.address, 'closed')
-      for client in clients:
-         client.sendMessage(self.address[0] + u' - disconnected')
-
 
 if __name__ == "__main__":
 
@@ -55,8 +35,6 @@ if __name__ == "__main__":
    (options, args) = parser.parse_args()
 
    cls = SimpleEcho
-   if options.example == 'chat':
-      cls = SimpleChat
 
    if options.ssl == 1:
       server = SimpleSSLWebSocketServer(options.host, options.port, cls, options.cert, options.key, version=options.ver)
